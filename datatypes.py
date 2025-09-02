7faacbe3bbe6288e9c9d508e47484eae6589e7dc
maggie-theOG <gal018@stge.org.uk>
2025-09-02 09:24:04 +0000

    datatypes

---
+++ datatypes.py
@@ -0,0 +1,35 @@
+name = "maggie" #this is a string
+myAge = 428 #a integer
+height = 5000.01 #this is a decimal number #a float/reel
+hasAnOakSappling = True #a boolean (true/false question)
+
+# casting (change from one data type to another)
+age = input("enter age: ")
+print(age)
+print(age + "10") # concatenate 2 strings together (join them together)
+ageAsAnInt = int(age)
+print(ageAsAnInt + 10) # off two integers together (maths addition)
+print("your age is " + str(ageAsAnInt))
+
+stillAnInteger = -4
+myNumber = "7984731791"
+
+# arithmatic
+add = 10 + 10
+subtract = 10 - 10
+multiply = 10 * 10
+divide = 11 / 10 # will output a float
+integerDivision = 10 // 10 
+print(add, subtract, multiply, divide, integerDivision) 
+
+modulo = 500 % 501 
+print(modulo)
+mystery = 2 ** 3 # to the power of 
+print(mystery)
+
+# activity 1 - take two inputs, multiply them together and output answer
+number1 = input("785932785749071758431") 
+number2 = input("986953078781678437453")
+multiply = int(number1) * (number2)
+# activity 2 - input users age, output age times 7
+# activity 3 - take radius as input, output volume of sphere (v = 4/3 x pi x r^3)
---
+++ songmadLibs.py
@@ -0,0 +1,18 @@
+year = input("enter a year: ")
+color = input("enter a color: ")
+weather = input("enter a weather: ")
+time = input("enter a time: ")
+time2 = input("enter a time2: ")
+typeofperson = input("enter a typeofperson: ")
+
+
+
+
+
+print("In the year of " + year + " assembled here the volunteers")
+print("In the days when lands were few")
+print("Here the ship sailed out into the " + color + " and " + weather + " morn")
+print("The sweetest sight ever seen")
+print("And the " + time + " followed " + time2 + "")
+print("and the " + typeofperson + " say")
+

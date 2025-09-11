2fe86b265719541cce8f0f4a2a4595400a571d05
maggie-theOG <gal018@stge.org.uk>
2025-09-11 11:04:39 +0000

    conditional loops

---
+++ loops/conditional loops/conditional loops.py
@@ -0,0 +1,17 @@
+# garettishappy = "Y"
+# while garettishappy == "Y":
+#     print("your happy bro")
+#     garettishappy = input("are you not endangered? (Y/N)")
+# print("SEEK HELP IMMEDIATLY")
+
+code = "sigmand fraud"
+userInput = input("enter code: ")
+while code != userInput:
+ print("thats so wrong, thats not even close you idiot, your pathetic, you cant even get the code you already know into the damn system, your a failure and you will never succeed ever now try again before someone hates on you forever!")
+   userInput = input("try again you absolute idiot: ")
+ print("finally you got it, you can prove to people your not a failure but your still an idiot and i hate you for wasting my time bro")
+
+
+# password = input(whats your password you stupid human?)
+# while len(password) < 5:
+#     password = input("come on stupid, AGAIN")

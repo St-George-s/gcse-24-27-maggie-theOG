b8afadc0438bb7e5d827f1f476c3f684f0cb031b
maggie-theOG <gal018@stge.org.uk>
2025-10-28 10:24:50 +0000

    files

---
+++ arrays/filehandling/readingafile.py
@@ -0,0 +1,6 @@
+#with open("/workspaces/gcse-24-27-maggie-theOG/arrays/filehandling/practiceread.txt", "r") as file:
+    #content = file.read()
+    #print(content)
+
+with open("/workspaces/gcse-24-27-maggie-theOG/arrays/filehandling/practiceread.txt", "a") as file:
+    file.write("\nthis is new line")
---
+++ arrays/filehandling/writingtoafile.py
@@ -0,0 +1,3 @@
+with open("practice.txt", "w") as file:
+    for counter in range(1001): 
+      file.write("what is good world\n")

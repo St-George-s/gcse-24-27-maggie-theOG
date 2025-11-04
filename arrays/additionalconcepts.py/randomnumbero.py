ded478e1329c3d373240170008abbb28ad2df158
maggie-theOG <gal018@stge.org.uk>
2025-11-04 10:26:02 +0000

    randomnumbero

---
+++ arrays/additionalconcepts.py/randomnumbero.py
@@ -0,0 +1,9 @@
+# import random
+# myrandom = random.randint(1, 100)
+# print(myrandom)
+
+import random
+myfirstdice = random.randint(1, 100)
+myseconddice = random.randint(1, 100)
+print(str(myfirstdice) +)
+
---
+++ arrays/additionalconcepts.py/stringmanipulation.py
@@ -0,0 +1,26 @@
+# coolstring = "ITS TIMEEEEEEEEEEE"
+# print(coolstring[0])
+# print(coolstring[1])
+# print(coolstring[2])
+# print(coolstring[3])
+# print(coolstring[4])
+# print(coolstring[5])
+# print(coolstring[6])
+# print(coolstring[7])
+# print(coolstring[8])
+# print(coolstring[9])
+# print(coolstring[10])
+# print(coolstring[11])
+
+#print(coolstring[67])
+
+# something = input("say something: ")
+
+# print(something[0])
+# print(something[-1])
+# reversedstring = ""
+# userstring = input("enter something mate: ")
+# for character in userstring:
+#     reversedstring = character + reversedstring
+
+# print(reversedstring)
